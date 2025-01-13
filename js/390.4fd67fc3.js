a6fff2a62380c7078e07ef669e1326e0c6023583
SarveshMina <105467665+SarveshMina@users.noreply.github.com>
2025-01-13 06:06:31 +0000

    Hosting in /dist.

---
+++ js/390.4fd67fc3.js
@@ -0,0 +1,7 @@
+"use strict";(self["webpackChunkcalendify_frontend"]=self["webpackChunkcalendify_frontend"]||[]).push([[390],{6390:function(n,t,e){e.r(t),e.d(t,{allDay:function(){return s},createEvent:function(){return k},dateFormat:function(){return h},day:function(){return d},default:function(){return m},deleteEvent:function(){return l},month:function(){return c},months:function(){return u},noEvent:function(){return y},today:function(){return i},week:function(){return f},weekDays:function(){return r},year:function(){return a},years:function(){return o}});
+/**
+  * vue-cal v4.10.0
+  * (c) 2024 Antoni Andre <antoniandre.web@gmail.com>
+  * @license MIT
+  */
+const r=["الإثنين","الثلاثاء","الأربعاء","الخميس","الجمعة","السبت","الأحد"],u=["يناير","فبراير","مارس","أبريل","مايو","يونيو","يوليو","أغسطس","سبتمبر","أكتوبر","نوفمبر"," ديسمبر"],o="سنوات",a="سنة",c="شهر",f="أسبوع",d="يوم",i="اليوم",y="لا حدث",s="طوال اليوم",l="حذف",k="إنشاء حدث",h="dddd D MMMM YYYY",m={weekDays:r,months:u,years:o,year:a,month:c,week:f,day:d,today:i,noEvent:y,allDay:s,deleteEvent:l,createEvent:k,dateFormat:h}}}]);
